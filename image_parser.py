46b217a1dbaab9d3144b1cb93ad60fe6bb686d46
Vale <valetolpegin@gmail.com>
2015-04-28 17:58:57 -0400

    Created pseudocode for image parser

---
+++ image_parser.py
@@ -0,0 +1,24 @@
+__author__ = 'Vale Tolpegin'
+
+class image_parser:
+    #image that needs to be parsed variable
+    
+    #shape #1
+    #shape #2
+    #shape #3
+
+    def __init__( self, *args, **kwargs ):
+        pass
+
+    def parse( picture_file ):
+        #set image variable here with the image file picture_file
+
+        #find shapes in image ( basic geometric shapes such as circles, squares, triangles, etc )
+
+        #inside of those geometric shapes, find the number of different colors
+        #if there are 2 colors ( no more and no less )
+            #compare the ratio of those 2 colors
+            #if that ratio is close to 2:1 ( shape:letter )
+                #return true
+
+        #return false
