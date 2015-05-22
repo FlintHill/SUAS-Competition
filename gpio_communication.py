009cf3984585d5b5318cbcb78b917b9f627ef91e
Vale <valetolpegin@gmail.com>
2015-05-22 09:46:42 -0400

    New class created & image update
    - Removed images that are not usable
    - Added new class that will handle communication between

---
+++ gpio_communication.py
@@ -0,0 +1,14 @@
+__author__ = 'Vale Tolpegin'
+
+import RPi.GPIO as GPIO
+
+#setting default GPIO board settings
+GPIO.setmode( GPIO.BOARD )
+
+class gpio_communication:
+    
+    def __init__( self, *args, **kwargs ):
+        pass
+
+if __name__ == '__main__':
+    pass
