997bd15cc779652441dc3af4e148f337c87fd3e5
Vale <valetolpegin@gmail.com>
2016-03-02 09:34:26 -0500

    Add methods for ADLC parser

--- SUASImageParser/ADLC/parser.py
+++ SUASImageParser/ADLC/parser.py
@@ -5,3 +5,31 @@ class ADLCParser:
 
     def __init__(self):
         pass
+
+    def simple_parse(self, image):
+        """
+        Returns true/false whether an in-depth parse should be applied. This
+        method only does a couple of quick checks to remove images where it is
+        clear there are no targets.
+        """
+        # @TODO: Implement simple parsing
+
+        return True
+
+    def parse(self, image):
+        """
+        Parses the given image to identify potential ADLC targets. Returns a
+        list of possible targets.
+        """
+        # @TODO: Implement parsing
+
+        pass
+
+    def identify_characteristics(self, target):
+        """
+        Identifies the characteristics of the target "target" and returns
+        them as a dictionary object
+        """
+        # @TODO: Implement characteristic identification
+
+        return {}
