aa475c6df23d94415b76657432ea9f64323c1f75
Vale <valetolpegin@gmail.com>
2016-03-03 12:13:07 -0500

    Move the main file

--- suasimageparser.py
+++ suasimageparser.py
@@ -1,15 +1,15 @@
 import numpy as np
 import cv2
 
-img = cv2.imread( '../images/IMG_0160.JPG' )
+img = cv2.imread( 'images/IMG_0160.JPG' )
 
 cv2.imshow( "Original", img )
 cv2.waitKey( 0 )
 cv2.destroyAllWindows()
 
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-smoothed = cv2.cvtColor( hsv, cv2.COLOR_BGR2GRAY )
+cleaned = cv2.cvtColor( hsv, cv2.COLOR_BGR2GRAY )
 
-cv2.imshow( "Smoothed", smoothed )
+cv2.imshow( "Cleaned", cleaned )
 cv2.waitKey( 0 )
 cv2.destroyAllWindows()
