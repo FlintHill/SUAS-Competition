8e0edabd56305111b6e0238c3672fede3290efa5
Vale <valetolpegin@gmail.com>
2016-06-26 20:49:55 -0400

    Try out a custom thresholding technique

--- SUASImageParser/ADLC/parser.py
+++ SUASImageParser/ADLC/parser.py
@@ -32,12 +32,16 @@ class ADLCParser:
         self.settings = settings
 
         # Setting up general settings
-        self.debug = settings.get("debug")
+        self.DEBUG = settings.get("debug")
 
         # Setting up all parsing specific settings
-        # @TODO: Calculate correct lower and upper areas
+        # @TODO: Calculate correct lower and bounds for target detection
         self.LOWER_CONTOUR_AREA = settings.get("LOWER_CONTOUR_AREA", 500)
         self.HIGHER_CONTOUR_AREA = settings.get("HIGHER_CONTOUR_AREA", 50000)
+
+        self.THRESH_TYPE = cv2.THRESH_BINARY
+        self.THRESH_VALUE = 127
+        self.THRESH_POS_VAL = 255
 
     def set_debug(self, updated_debug):
         """
@@ -45,7 +49,7 @@ class ADLCParser:
         whether status updates should be printed to terminal throughout
         the parsing process.
         """
-        self.debug = updated_debug
+        self.DEBUG = updated_debug
 
     def parse(self, filename):
         """
@@ -53,7 +57,7 @@ class ADLCParser:
         list of possible targets.
         """
         # If debugging is enabled, setting the time started
-        if self.debug:
+        if self.DEBUG:
             self.start_time = timeit.default_timer()
 
         # Load image
@@ -61,29 +65,29 @@ class ADLCParser:
 
         # Convert image to HSV then to GRAY to help make it easy to identify
         #   possible targets
-        if self.debug:
+        if self.DEBUG:
             print(bcolors.INFO + "[Info]" + bcolors.ENDC + " Converting image to HSV")
         hsv = cv2.cvtColor(self.image.get_image(), cv2.COLOR_BGR2HSV)
 
-        if self.debug:
+        if self.DEBUG:
             print(bcolors.INFO + "[Info]" + bcolors.ENDC + " Applying histogram equalization")
         equalized = histogram_equalization.histeq(hsv)[0].astype(np.uint8)
 
-        if self.debug:
+        if self.DEBUG:
             print(bcolors.INFO + "[Info]" + bcolors.ENDC + " Converting HSV to GRAY")
         img = cv2.cvtColor(equalized, cv2.COLOR_BGR2GRAY)
 
-        if self.debug:
+        if self.DEBUG:
             print(bcolors.INFO + "[Info]" + bcolors.ENDC + " Inverting the image")
         img = (255-img)
 
         # Identifying targets
-        if self.debug:
+        if self.DEBUG:
             print(bcolors.INFO + "[Info]" + bcolors.ENDC + " Identifying targets")
         targets = self.identify_targets(img)
 
         # Identifying target characteristics
-        if self.debug:
+        if self.DEBUG:
             print(bcolors.INFO + "[Info]" + bcolors.ENDC + " Identifying target characteristics")
         target_characteristics = None
         if len(targets) > 0:
@@ -96,7 +100,7 @@ class ADLCParser:
                 cv2.destroyAllWindows()
 
         # If debugging is enabled, calculating time took to parse
-        if self.debug:
+        if self.DEBUG:
             end_time = timeit.default_timer()
             print(bcolors.INFO + "[Info]" + bcolors.ENDC + " Took " + str(end_time - self.start_time) + " seconds to parse the image")
 
@@ -112,16 +116,21 @@ class ADLCParser:
         targets = []
 
         # Thresholding image
-        if self.debug:
+        if self.DEBUG:
             print(bcolors.INFO + "[Info]" + bcolors.ENDC + " Thresholding the image")
         # @TODO: Figure out a way to optimize this process to pick the correct
         #   thresholding method & settings
-        ret,thresh = cv2.threshold(img,127,255,0)
-        #ret,thresh = cv2.threshold(img,127,255,cv2.THRESH_TRUNC)
+        #ret,thresh = cv2.threshold(img, self.THRESH_VALUE, self.THRESH_POS_VAL, self.THRESH_TYPE)
         #thresh = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,14)
+        #thresh = cv2.Canny(img,175,225)
+        thresh = self.new_thresh(img)
+
+        cv2.imshow("target", thresh)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
         # Getting contours
-        if self.debug:
+        if self.DEBUG:
             print(bcolors.INFO + "[Info]" + bcolors.ENDC + " Getting the contours of objects")
         # @TODO: Investigate possibly using a different method to avoid this.
         #   Line detection might be a much better route to go (if used in
@@ -138,12 +147,29 @@ class ADLCParser:
 
                 is_a_target, possible_target = self.parse_possible_target(img, cnt)
                 if is_a_target:
-                    if self.debug:
+                    if self.DEBUG:
                         print(bcolors.INFO + "[Info]" + bcolors.ENDC + " Found a target")
                     targets.append(possible_target)
 
         # Return the identified targets
         return targets
+
+    def new_thresh(self, img, LOWER_THRESH=105, UPPER_THRESH=255):
+        """
+        Custom thresholding method to help normalize the image. Pixels with colors
+        between the LOWER_THRESH & UPPER_THRESH are turned black, while the outer
+        regions are turned white.
+        """
+        for x in xrange(0, img.shape[0], 1):
+            for y in xrange(0, img.shape[1], 1):
+                if img[x, y] > LOWER_THRESH and img[x, y] < UPPER_THRESH:
+                    img[x, y] = 255
+                else:
+                    img[x, y] = 0
+
+            print(str(x) + " out of " + str(img.shape[0]))
+
+        return img
 
     def parse_possible_target(self, img, contours):
         """
@@ -175,6 +201,8 @@ class ADLCParser:
         false if it is not.
         """
         approx_sides = cv2.approxPolyDP(contours, 0.15 * cv2.arcLength(contours, True), True)
+        # NOTE: This will have to be rewritten because of the possibility of a cross, hexagon,
+        #   etc. being present at competition
         if len(approx_sides) != 5 and len(approx_sides) != 4 and len(approx_sides) != 3:
             return False
 
