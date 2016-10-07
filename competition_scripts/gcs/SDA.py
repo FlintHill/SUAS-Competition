c59101d175e70f448cb7a19809548fa69d0d82d4
JackHarrington <jharrington@flinthill.org>
2016-10-07 09:13:43 -0400

    added obstacle class, to-do methods, and telemetry reporting thread. fixed formatting issues

--- competition_scripts/gcs/SDA.py
+++ competition_scripts/gcs/SDA.py
@@ -7,7 +7,7 @@
 	@authors	jack harrington, james villemarette, vale tolpegin
 	@date		4-10-2016
 """
-import sys, logging, clr, time, MissionPlanner
+import sys, thread, logging, clr, time, MissionPlanner
 clr.AddReference("MissionPlanner.Utilities")
 
 # CLASS
@@ -36,9 +36,19 @@ class Obstacle:
 # VARIABLES
 
 drone_radius = 5 # meters
+safety_radius = 5 # for all obstacles
 
 # FUNCTIONS
-def new_waypoint(lat, lng, alt):
+def reportTelemetry(threadName, delay):
+	"""
+	Post to the interoptclient.py our current position
+	"""
+	# something should be replaced with link to interopclient.py
+	# something.post_telemetry(self, cs.lat, cs.lng, cs.alt, cs.yaw)
+	# print "Reported Position"
+	# time.sleep(delay)
+
+def newWaypoint(lat, lng, alt):
 	"""
 	Send a new waypoint in guided mode to go to
 	"""
@@ -49,19 +59,19 @@ def new_waypoint(lat, lng, alt):
 	MAV.setGuidedModeWP(item)
 	print "Waypoint which is LAT:%s LONG:%s ALT:%s" % (lat, lng, alt)
 
-def meters_to_feet(m):
+def metersToFeet(m):
 	"""
 	Convert meters to feet
 	"""
 	return m * 3.28
 
-def feet_to_meters(ft):
+def feetToMeters(ft):
 	"""
 	Convert feet to meters
 	"""
 	return ft / 3.28
 
-def reach_waypoint(lat, lng, alt, range, altrange):
+def reachLocation(lat, lng, alt, range, altrange):
 	"""
 	Stalls script until the drone reaches a certain lat, long, and altitude within a range
 		range = latitude, longitude range
@@ -75,43 +85,59 @@ def reach_waypoint(lat, lng, alt, range, altrange):
 			return True
 		time.sleep(0.1)
 
-def calculate():
+def calculateStationary():
 	"""
-	Calculates within a 3D space if the drone will collide 
+	Calculates if the drone will collide within a 2D space.
+		returns x >= 0 if obstacle is within the drones flight path,
+		x is the id of the obstacle
+
+		returns -1 if no obstacle detected
 	"""
 
-def calculate_entity_path():
+
+def calculateEntityPath():
 	"""
 
 	"""
 
-def draw_obstacle(id, lat, lng, alt):
+def drawObstacle(id, lat, lng, alt):
 	"""
 	Draws or redraws (updates) an obstacle on Mission Planner
 	"""
 
-def dodge_stationary():
+def dodgeStationary(obstacleID):
 	"""
 	
 	"""
 
-def dodge_motion():
+def dodgeMotion():
 	"""
-
+	
 	"""
 
 # RUNTIME CODE
-"""
+
+try:
+	thread.start_new_thread( report_telemtry, ("Position-Thread", 0.1, ) )
+except:
+	print "Error: unable to start thread"
+
+
+
 while True:
-	if calculate():
+	if calculateStationary() > -1:
 		# if a collision is imminent
-		#x
-	else
+		dodgeStationary(calculateStationary())
+	else if calculateMotion() > -1:
+		# if a collision is imminent
+		dodgeMotion(calculateMotion())
+	else:
 		# if no collision is present
 		time.sleep(100)
-"""
+
 
 # SETUP
+"""
 print "Starting Mission"
 
 Script.ChangeMode("Guided")
@@ -134,3 +160,5 @@ print "Auto Mode"
 
 print "End of Script"
 time.sleep(99999)
+
+"""
