5b7150d17c2a86e6b229cdf624762ba28792ad1e
pete2fiddy <peterhusisian@gmail.com>
2016-10-20 09:30:25 -0400

    committed to add K-Means implementation

--- PeterDev/Main.py
+++ PeterDev/Main.py
@@ -10,10 +10,11 @@ import math
 import numpy
 from matplotlib.pyplot import colors
 from SystemEvents.Standard_Suite import color
+from Carbon.Aliases import true
+import sys
 
 
-
-img = Image.open("/Users/phusisian/Desktop/Senior year/SUAS/Object images/targets 300 downScaled2.jpeg")
+img = Image.open("/Users/phusisian/Desktop/Senior year/SUAS/Object images/targets 300 downScaled.jpg")
 #img.show()
 image = img.load()
 dim = img.size
@@ -45,6 +46,29 @@ class EdgeDetection:
                 
                 dColor[x][y] = (math.sqrt(dr1+dg1+db1) + math.sqrt(dr2+dg2+db2))/2.0
         return dColor      
+    
+    def getDColorFromKMeans(self,imgIn, numRounds):
+        imageIn = imgIn.load()
+        roundAmount = 255/numRounds
+        dColor = [[0 for i in range(imgIn.size[1])] for j in range(imgIn.size[0])]
+        height = imgIn.size[1]
+        width = imgIn.size[0]
+        for y in range(1,height):
+            for x in range(1,width):
+                currentColor = imageIn[x,y]
+                leftColor = imageIn[x-1,y]
+                topColor = imageIn[x,y-1]
+                dr1 = (float(currentColor[0]-leftColor[0])/float(roundAmount))**2
+                dg1 = (float(currentColor[1]-leftColor[1])/float(roundAmount))**2
+                db1 = (float(currentColor[2]-leftColor[2])/float(roundAmount))**2
+                
+                dr2 = (float(currentColor[0]-topColor[0])/float(roundAmount))**2
+                dg2 = (float(currentColor[1]-topColor[1])/float(roundAmount))**2
+                db2 = (float(currentColor[2]-topColor[2])/float(roundAmount))**2
+                
+                dColor[x][y] = (math.sqrt(dr1+dg1+db1) + math.sqrt(dr2+dg2+db2))/2.0
+        return dColor      
+    
     
     def getSobelEdgeImage(self, dColor, renderType, threshold):
         img = Image.new('RGB', ((len(dColor)), len(dColor[0])))
@@ -271,6 +295,58 @@ class ImageEdit:
 
 class ObjectDetection:
     
+    def removeBlack(self, imgIn, threshold):
+        image = imgIn.load()
+        for x in range(1, imgIn.size[0]):
+            for y in range(1, imgIn.size[1]):
+                amountOff = math.sqrt(image[x,y][0]**2 + image[x,y][1]**2 + image[x,y][2]**2)
+                if amountOff < threshold:
+                    image[x,y]=(image[x-1,y][0], image[x-1,y][1], image[x-1,y][2])
+        return imgIn
+    def KMeans(self, imgIn, numRounds):
+        image = imgIn.load()
+        roundAmount = 255/numRounds
+        for x in range(0, imgIn.size[0]):
+            for y in range(0, imgIn.size[1]):
+                red = int(round(image[x,y][0]/roundAmount))*roundAmount
+                green = int(round(image[x,y][1]/roundAmount))*roundAmount
+                blue = int(round(image[x,y][2]/roundAmount))*roundAmount
+                image[x,y]=(red,green,blue)
+        return imgIn
+    
+    def KMeans2(self, imgIn, numRounds):
+        pointArr=[0 for i in range(0,numRounds)]
+        for i in range(1,len(pointArr)+1):
+            pointArr[i-1]=(1.01**((557.286/float(numRounds))*i))-1
+        print(pointArr)
+        image = imgIn.load()
+        for x in range(0, imgIn.size[0]):
+            for y in range(0, imgIn.size[1]):
+                red = 0 
+                green = 0
+                blue = 0
+                for i in range(1,len(pointArr)):
+                    if image[x,y][0] > pointArr[i-1] and image[x,y][0] < pointArr[i]:
+                        red = int(pointArr[i-1])
+                    if image[x,y][1] > pointArr[i-1] and image[x,y][1] < pointArr[i]:
+                        green = int(pointArr[i-1])
+                    if image[x,y][2] > pointArr[i-1] and image[x,y][2] < pointArr[i]:
+                        blue = int(pointArr[i-1])
+                
+                image[x,y]=(red,green,blue)
+        return imgIn
+    
+    def flattenColors(self, imgIn, dColor, threshold):
+        image = imgIn.load()
+        for x in range(1, imgIn.size[0]-1):
+            for y in range(1, imgIn.size[1]-1):
+                if dColor[x][y] < threshold and dColor[x-1][y] < threshold:
+                    red = image[x-1,y][0]
+                    green = image[x-1,y][1]
+                    blue = image[x-1,y][2]
+                    image[x,y]=(red,green,blue)
+        return imgIn
+    
     #doesn't work as well as boundObjectsAVG
     def boundObjects(self, imgIn, dColor, percentThreshold, threshold, chunksWidth, chunksHeight):
         image = imgIn.load()
@@ -292,6 +368,14 @@ class ObjectDetection:
                     #color = (int(255*numOff),0,255-int(255*numOff))
                     #ie.fillRect(imgIn, x*chunkWidth, y*chunkHeight, chunkWidth, chunkHeight, color)
         return imgIn
+    
+    def getSolidColorBoolean(self, dColor,threshold):
+        vals = [[False for j in range(0,len(dColor[0]))] for i in range(0,len(dColor))]
+        for x in range(0, len(dColor)):
+            for y in range(0, len(dColor[0])):
+                if dColor[x][y] < threshold:
+                    vals[x][y]=True
+        return vals
     
     def boundObjectsAVG(self, imgIn, dColor, threshold, chunksWidth, chunksHeight):
         image = imgIn.load()
@@ -372,15 +456,61 @@ class ObjectDetection:
                     add += listIn[i][j]
             return float(float(add)/float(area))
         return 0
+    
+    def fillShapeChunks(self, imgIn, objArr, chunksWidth, chunksHeight, percentChunk):
+        objDetect = ObjectDetection()
+        ie = ImageEdit()
+        chunkWidth = imgIn.size[0]/chunksWidth
+        chunkHeight = imgIn.size[1]/chunksHeight
+        objArr=numpy.array(objArr)
+        for x in range(0, chunksWidth-1):
+            for y in range(0, chunksHeight-1):
+                ie.drawRect(imgIn, x*chunkWidth, y*chunkHeight, chunkWidth-1, chunkHeight-1, (0,0,255))
+                if objDetect.percentTrue2D(objArr[(x*chunkWidth):(x*chunkWidth + chunkWidth),(y*chunkHeight):(y*chunkHeight + chunkHeight)]) > percentChunk:
+                    ie.drawRect(imgIn, x*chunkWidth, y*chunkHeight, chunkWidth-1, chunkHeight-1, (255,0,0))
+        return imgIn
+    
+    def percentTrue2D(self, listIn):
+        add = 0
+        area = len(listIn)*len(listIn[0])
+        for i in range(0, len(listIn)):
+            for j in range(0, len(listIn[0])):
+                if listIn[i][j] == True:
+                    add +=1
+        return float(float(add)/float(area))
+    def fillAllTrue(self, imgIn, objArr):
+        image = imgIn.load()
+        for x in range(0,len(objArr)):
+            for y in range(0, len(objArr[0])):
+                if objArr[x][y] == True:
+                    image[x,y] = (255,0,0)
+        return imgIn
 start = time.time()
 edgeDetection = EdgeDetection()
-dColor = edgeDetection.getDColor(img)
+#dColor = edgeDetection.getDColor(img)
+#edgeDetection.removeLowerDColorThreshold(dColor, 3)
 objDetect = ObjectDetection()
-edgeDetection.removeLowerDColorThreshold(dColor, 20)
-d2ColorX = edgeDetection.getD2ColorX(dColor)
-d2ColorY = edgeDetection.getD2ColorY(dColor)
-b=edgeDetection.getD2EdgeImage(d2ColorX, d2ColorY)
-objArr = objDetect.getObjArrayUsingDColorUnder(dColor, 3, 8)
+#dColor1 = edgeDetection.getDColor(img)
+b=objDetect.KMeans(img, 8)
+dColor2 = edgeDetection.getDColorFromKMeans(b, 8)
+for j in range(0, len(dColor2[0])/10):
+    for i in range(0, len(dColor2)/10):
+        print(dColor2[i*10][j*10]),
+        print(", "),
+    print()
+#b=objDetect.boundObjectsAVG(b, dColor2, 0.3, 720, 480)
+#dColor2 = edgeDetection.getDColor(b)
+#objArr=objDetect.getSolidColorBoolean(dColor2, 3)
+#b=objDetect.fillShapeChunks(b, objArr, 480, 360, 0.6)
+#objArr=objDetect.getSolidColorBoolean(dColor1, dColor2, 5)
+#b=objDetect.fillAllTrue(b, objArr)
+#b=objDetect.KMeans(b, 6)
+#b = objDetect.flattenColors(b, dColor, 5)
+#edgeDetection.removeLowerDColorThreshold(dColor, 20)
+#d2ColorX = edgeDetection.getD2ColorX(dColor)
+#d2ColorY = edgeDetection.getD2ColorY(dColor)
+#b=edgeDetection.getD2EdgeImage(d2ColorX, d2ColorY)
+#objArr = objDetect.getObjArrayUsingDColorUnder(dColor, 3, 8)
 #b=objDetect.fillObjArr(b, objArr, 480, 320, 0.85)
 #b=edgeDetection.getSobelEdgeImage(dColor, "SOFT_EDGES_THRESHOLD", 70)
 #b=edgeDetection.getColorCodedImage(dColor, "COLOR_CODE_NORMAL", 15)
