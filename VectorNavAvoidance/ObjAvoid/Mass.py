d8570d9655bd25c0c8781634b91f7ec403cb98c2
pete2fiddy <peterhusisian@gmail.com>
2017-01-11 09:36:07 -0500

    added new vector nav avoidance

---
+++ VectorNavAvoidance/ObjAvoid/Mass.py
@@ -0,0 +1,46 @@
+'''
+Created on Jan 10, 2017
+
+@author: phusisian
+'''
+from ObjAvoid.Vector import Vector
+from ObjAvoid.MassHolder import MassHolder
+from ObjAvoid.graphics import *
+from ObjAvoid.Window import Window
+class Mass:
+    '''timeStep is the amount of time for which the force is being applied'''
+    #DEFAULT_TIMESTEP = .1 #in Ms
+    def __init__(self, boundMassHolder, pointIn, massIn):
+        self.boundMassHolder = boundMassHolder
+        self.point = pointIn
+        self.mass = massIn
+        #self.velocityVector = Vector([0,0,0])
+      
+    def getForceVectorToMass(self, massTwo):
+        magForce = self.getMagnitudeOfForceToMass(massTwo)
+        forceUnitVector = self.getVectorToMass(massTwo).getUnitVector()
+        return forceUnitVector * magForce
+    
+    
+    
+    def getMagnitudeOfForceToMass(self, massTwo):    
+        return  ((massTwo.getMass() * self.mass)/(self.getVectorToMass(massTwo).getMagnitude())**2) * MassHolder.GRAVITY_CONSTANT
+    
+    def getVectorToMass(self, massTwo):
+        return self.point.getVectorToPoint(massTwo.getPoint()) 
+    
+        
+    def getMass(self):
+        return self.mass
+    
+    def getPoint(self):
+        return self.point
+    
+    def draw(self, win):
+        c = Circle(Point(int(self.point[0] + win.getCenterPoint()[0]), int(win.getCenterPoint()[1] - self.point[1])), 10)#subtracted so reversed y axis graphics work. 
+        c.draw(win.getGraphWin())
+    
+    def __repr__(self):
+        returnString = "Mass at: " + str(self.point)
+        return returnString
+    
---
+++ VectorNavAvoidance/ObjAvoid/Window.py
@@ -0,0 +1,35 @@
+'''
+Created on Jan 10, 2017
+
+@author: phusisian
+'''
+from ObjAvoid.graphics import *
+from time import sleep
+
+class Window:
+    
+    
+    REFRESH_TIME = .1
+    def __init__(self, drawable, dim):
+        self.dim = dim
+        self.drawable = drawable
+        self.graphWin = GraphWin("Masses", self.dim[0], self.dim[1])
+        self.centerPoint = (720, 510)
+        self.drawLoop()
+    
+    def getGraphWin(self):
+        return self.graphWin
+    
+    def getCenterPoint(self):
+        return self.centerPoint
+    
+    def drawLoop(self):
+        while(True):
+            self.drawable.draw(self)
+            sleep(Window.REFRESH_TIME)
+            coverRect = Rectangle(Point(0, 0), Point(self.dim[0], self.dim[1]))
+            #coverRect.setFill("white")
+            #coverRect.draw(self.graphWin)
+        
+    
+    
