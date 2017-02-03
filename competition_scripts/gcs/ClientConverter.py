b15950d29e794e9b528df30657561421f2b6c4ce
pete2fiddy <peterhusisian@gmail.com>
2017-02-03 09:25:49 -0500

    made ClientConverter work with the wrapper classes Vale made

--- competition_scripts/gcs/ClientConverter.py
+++ competition_scripts/gcs/ClientConverter.py
@@ -11,7 +11,7 @@ from math import sin, cos
 
 class ClientConverter(object):
 
-    '''initialCoordinates will be current_coordinates from client class'''
+    '''initialCoordinates will be CurrentCoordinates object'''
     '''the role of the class is to funnel data from the client script into forms that are usable with my code.
     currently updates the position of the drone mass, but is set up to update other masses as well if it were
     to be funneled them fairly simply'''
@@ -26,19 +26,19 @@ class ClientConverter(object):
 
 
     def initMainDroneMass(self):
-        self.mainDroneMass = DroneMass(self.massHolder, MultiDimPoint([0,0,self.initialCoordinates[2]]), DroneMass.DEFAULT_DRONE_MASS)
+        self.mainDroneMass = DroneMass(self.massHolder, MultiDimPoint([0,0,self.initialCoordinates.get_altitude()]), DroneMass.DEFAULT_DRONE_MASS)
 
 
-    ''''needs to take coordinates in list that includes: altitude, haversine distance, and heading,
-    (in this order: haversine distance, heading, altitude)
-    with haversine distance and bearing calcuated from ClientConverter's initial coordinates, which can be pulled
-    from "getInitialCoordinates()"'''
+    '''takes an converter_data_update object that wraps altitude, haversine distance, and heading from the current position
+    to the position the drone needs to travel.
+    
+    '''
     def updateMainDroneMass(self, updateData):
-        dy = updateData[0] * sin(updateData[1])#where DY is change in y from initial GPS point
-        dx = updateData[0] * cos(updateData[1])#where DX is change in x from initial GPS point
-        newDronePoint = MultiDimPoint([dy, dx, updateData[2]])
+        dy = updateData.get_haversine_distance() * sin(updateData.get_heading())#where DY is change in y from initial GPS point
+        dx = updateData.get_haversine_distance() * cos(updateData.get_heading())#where DX is change in x from initial GPS point
+        newDronePoint =  MultiDimPoint([dy, dx, updateData.get_altitude()])
         self.mainDroneMass.setPoint(newDronePoint)
 
 
-    '''reminder not to switch to guided mode unless tgher are nearby obstacles nearby. Search all the masses
+    '''reminder not to switch to guided mode unless there are nearby obstacles nearby. Search all the masses
     in massHolder to see if any are close enough that they should be avoided'''
