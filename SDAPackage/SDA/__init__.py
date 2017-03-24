4a47e6ea044b6527dae223f62314ab05f251c251
Vale <valetolpegin@gmail.com>
2017-03-24 15:36:03 -0400

    Add moving obstacle for SDA package

--- SDAPackage/SDA/__init__.py
+++ SDAPackage/SDA/__init__.py
@@ -3,5 +3,6 @@ from .obstacle import Obstacle
 from .vector_math import VectorMath
 from .waypoint_holder import WaypointHolder
 from .stationary_obstacle import StationaryObstacle
+from .moving_obstacle import MovingObstacle
 from .drone import Drone
 from .obstacle_map import ObstacleMap
---
+++ SDAPackage/SDA/moving_obstacle.py
@@ -0,0 +1,29 @@
+from SDA import Obstacle
+from SDA import Constants
+
+class MovingObstacle(Obstacle):
+    """
+    Wrapper class for Moving obstacles
+    """
+
+    def __init__(self, point, radius, speed):
+        """
+        :param point: The point for the moving obstacle
+        :type point: Numpy Array
+        """
+        super(StationaryObstacle, self).__init__(point, Constants.STATIONARY_OBSTACLE_SAFETY_RADIUS)
+
+        self.radius = radius
+        self.speed = speed
+
+    def get_radius(self):
+        """
+        Return the radius of the obstacle plus that of the safety radius
+        """
+        return (self.radius + self.get_safety_radius()) * self.speed / 10.0
+
+    def get_speed(self):
+        """
+        Returns the speed of the obstacle
+        """
+        return self.speed
