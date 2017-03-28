ce9a3707d5809e7d713e9bd9c7295ba8e0fb1cef
Vale <valetolpegin@gmail.com>
2017-03-27 20:52:10 -0400

    Rename the image processing library, make some minor improvements to SyntheticTester

--- ImgProcessingCLI/ImgProcessingCLI/Testing/SyntheticTester.py
+++ ImgProcessingCLI/ImgProcessingCLI/Testing/SyntheticTester.py
@@ -6,10 +6,16 @@ from EigenFit import *
 import timeit
 
 class SyntheticTester(object):
+
     def __init__(self, img_path_in, data_path_in, scale, extension):
         self.img_path = img_path_in
         self.data_path = data_path_in
         self.extension = extension
+        self.scale = scale
+
+        self.init_img_files()
+        self.init_data_files()
+        self.init_score_vals()
 
         print("categorizer initialization started")
         base_path = "/Users/vtolpegin/Desktop/SUAS/NEWLETTERPCA"
@@ -17,13 +23,10 @@ class SyntheticTester(object):
         projections_path = base_path + "/Data/Projections"
         mean = load_numpy_arr(base_path + "/Data/Mean/mean_img 0.npy")
         num_dim = 20
-        #nnet = Layers.init_from_files("/Users/phusisian/Desktop/Senior year/SUAS/NNet Files/Letter Weights 10 With Flipped Output", Sigmoid, [num_dim, 100, 27], Cost)
         self.letter_categorizer = Categorizer(eigenvectors, mean, projections_path, KMeansCompare, 25)
         print("categorizer initialization finished.")
-        self.init_img_files()
-        self.init_data_files()
-        self.init_score_vals()
-        self.check_answers(scale, extension)
+
+        self.test_set()
 
     def init_img_files(self):
         self.img_files = os.listdir(self.img_path)
@@ -48,40 +51,49 @@ class SyntheticTester(object):
     def init_score_vals(self):
         self.score_vals = numpy.zeros((5))
 
-    def check_answers(self, scale, extension):
+    def test_set(self):
         self.num_crashes = 0
-        for i in range(0, len(self.img_files)):
+        for index in range(0, len(self.img_files)):
             try:
-                start_time = timeit.default_timer()
-                shape_img = Image.open(self.img_path + "/" + str(self.img_files[i]))#Image.open(self.img_path + "/Generated Target " + str(i) + extension)
-
-                end_num = self.get_file_name_end_num(self.img_files[i])
-                matching_data_file_name = self.get_data_file_name_with_end_num(end_num)
-                scores = numpy.load(self.data_path + "/" + matching_data_file_name)
-
-                shape_img = shape_img.resize((int(shape_img.size[0]*scale), int(shape_img.size[1]*scale)))
-                target = Target(shape_img, shape_img.load(), self.letter_categorizer)
-                target_answers = target.as_numpy()
-
-                if str(scores[0]) == str(target_answers[0]):
-                    self.score_vals[0] += 1
-                elif self.get_if_letter_is_bidirectional(str(scores[3])):
-                    flipped_angle = self.get_compass_angle_180_degrees_away(str(target_answers[0]))
-                    if str(flipped_angle) == str(scores[0]):
-                        self.score_vals[0] += 1
-                print("=============================================================")
-                print("answer key: " + str(scores))
-                print("target answers: " + str(target_answers))
-                for answer_index in range(1, scores.shape[0]):
-                    if str(target_answers[answer_index]) == str(scores[answer_index]):
-                        self.score_vals[answer_index] += 1
-                print("current score vals: " + str(self.score_vals) + ", num images run: " + str(i+1))
-                print("time taken: " + str(timeit.default_timer() - start_time) + " seconds")
-                print("=============================================================")
-                print("------------------------------------------------------------")
+                self.run_img(index)
             except:
                 self.num_crashes += 1
-                print("done messt up")
+                print("Crashed during test_set() on img", self.img_files[index])
+
+    def run_img(self, index):
+        """
+        Process an image
+
+        :param index: The index of the image in self.img_files
+        :type index: int
+        """
+        start_time = timeit.default_timer()
+        shape_img = Image.open(self.img_path + "/" + str(self.img_files[index]))
+
+        end_num = self.get_file_name_end_num(self.img_files[index])
+        matching_data_file_name = self.get_data_file_name_with_end_num(end_num)
+        scores = numpy.load(self.data_path + "/" + matching_data_file_name)
+
+        shape_img = shape_img.resize((int(shape_img.size[0]*self.scale), int(shape_img.size[1]*self.scale)))
+        target = Target(shape_img, shape_img.load(), self.letter_categorizer)
+        target_answers = target.as_numpy()
+
+        if str(scores[0].decode("utf-8")) == str(target_answers[0]):
+            self.score_vals[0] += 1
+        elif self.get_if_letter_is_bidirectional(str(scores[3])):
+            flipped_angle = self.get_compass_angle_180_degrees_away(str(target_answers[0]))
+            if str(flipped_angle) == str(scores[0]):
+                self.score_vals[0] += 1
+        print("=============================================================")
+        print("answer key: " + str(scores))
+        print("target answers: " + str(target_answers))
+        for answer_index in range(1, scores.shape[0]):
+            if str(target_answers[answer_index].decode("utf-8")) == str(scores[answer_index]):
+                self.score_vals[answer_index] += 1
+        print("current score vals: " + str(self.score_vals) + ", num images run: " + str(index+1))
+        print("time taken: " + str(timeit.default_timer() - start_time) + " seconds")
+        print("=============================================================")
+        print("------------------------------------------------------------")
 
     def get_file_name_end_num(self, img_name):
         space_index = img_name.rfind(" ")
