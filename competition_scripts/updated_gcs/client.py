2cb33e6eea034febb175d20338858a867d55f8d3
Vale <valetolpegin@gmail.com>
2017-04-13 15:36:10 -0400

    Update the SDA Viewer

--- competition_scripts/updated_gcs/client.py
+++ competition_scripts/updated_gcs/client.py
@@ -9,14 +9,15 @@ from interop_client import InteropClientConverter
 from sda_converter import SDAConverter
 from converter_functions import *
 from sda_viewer import SDAViewSocket
+from SimpleWebSocketServer import SimpleWebSocketServer
 from vehicle_state import VehicleState
 from SDA import *
 
 TK1_ADDRESS = ('192.168.1.6', 9001)
 
-UAV_CONNECTION_STRING = "tcp:127.0.0.1:14551"
+UAV_CONNECTION_STRING = "tcp:127.0.0.1:5760"
 
-INTEROP_URL = "http://10.10.130.2:8000"
+INTEROP_URL = "http://10.10.130.101:8000"
 INTEROP_USERNAME = "Flint"
 INTEROP_PASSWORD = "2429875295"
 
@@ -38,15 +39,20 @@ def read_data(connection):
 
     return data
 
-def sda_viewer_process(vehicle_state_data, mission_data):
+def sda_viewer_process(logger_queue, configurer, vehicle_state_data, mission_data):
     """
     Creates a process for the SDA viewer
 
     :param vehicle_state_process: The vehicle's current state
     :type vehicle_state_process: multiprocessing.Array
     """
+    configurer(logger_queue)
+    name = multiprocessing.current_process().name
+
+    log(name, "Instantiating SDA Viewer process")
     sda_viewer_server = SimpleWebSocketServer('', 8000, SDAViewSocket, vehicle_state_data, mission_data)
     sda_viewer_server.serveforever()
+    log(name, "SDA Viewer process instantiated")
 
 def target_listener(logger_queue, configurer, received_targets_array):
     """
@@ -79,8 +85,8 @@ if __name__ == '__main__':
 
     vehicle_state_data = manager.list()
     mission_data = manager.list()
-    # sda_viewer_process = multiprocessing.Process(target=sda_viewer_process, args=(vehicle_state_data, mission_data))
-    # sda_viewer_process.start()
+    sda_viewer_process = multiprocessing.Process(target=sda_viewer_process, args=(logger_queue, logger_worker_configurer, vehicle_state_data, mission_data))
+    sda_viewer_process.start()
 
     logger_worker_configurer(logger_queue)
     name = multiprocessing.current_process().name
@@ -106,12 +112,14 @@ if __name__ == '__main__':
     sda_converter = SDAConverter(get_location(vehicle))
     sda_converter.set_waypoint(Location(38.8703041, -77.3214035, 100))
     #sda_converter.set_waypoint(Location(waypoints[0].x, waypoints[0].y, waypoints[0].z))
+    log(name, "SDA enabled")
     vehicle_state_data.append(get_vehicle_state(vehicle, sda_converter))
     mission_data.append(get_mission_json(interop_server_client.get_active_mission()))
 
+    log(name, "Everything is instantiated...Beginning operation")
     #try:
     while True:
-        current_location = get_location(vehicle)
+        """current_location = get_location(vehicle)
         current_waypoint_number = vehicle.commands.next
         current_uav_waypoint = waypoints[current_waypoint_number]
         sda_converter.set_waypoint(Location(current_uav_waypoint.x, current_uav_waypoint.y, current_uav_waypoint.z))
@@ -122,12 +130,13 @@ if __name__ == '__main__':
         for stationary_obstacle in stationary_obstacles:
             sda_converter.add_obstacle(get_obstacle_location(stationary_obstacle), stationary_obstacle)
         for moving_obstacle in moving_obstacles:
-            sda_converter.add_obstacle(get_obstacle_location(moving_obstacle), moving_obstacle)
+            sda_converter.add_obstacle(get_obstacle_location(moving_obstacle), moving_obstacle)"""
 
         vehicle_state_data[0] = get_vehicle_state(vehicle, sda_converter)
         mission_data[0] = get_mission_json(interop_server_client.get_active_mission())
 
-        if vehicle.mode.name == "GUIDED" and sda_converter.has_uav_reached_guided_waypoint():
+        sleep(0.5)
+        """if vehicle.mode.name == "GUIDED" and sda_converter.has_uav_reached_guided_waypoint():
             vehicle.mode = VehicleMode("AUTO")
 
         sda_converter.set_uav_position(current_location)
@@ -136,6 +145,6 @@ if __name__ == '__main__':
         print(current_location.get_altitude())
         if obj_avoid_coordinates:
             log("root", "Avoiding obstacles...")
-            vehicle.simple_goto(obj_avoid_coordinates)
+            vehicle.simple_goto(obj_avoid_coordinates)"""
     #except:
     #    pass#vehicle.close()
--- competition_scripts/updated_gcs/sda_viewer.py
+++ competition_scripts/updated_gcs/sda_viewer.py
@@ -6,23 +6,24 @@ class SDAViewSocket(WebSocket):
         """
         Initialize
         """
-        super(self, SDAViewSocket).__init__(p1, p2, p3)
+        super(SDAViewSocket, self).__init__(p1, p2, p3)
 
         self.vehicle_state_data = vehicle_state_data
         self.mission_data = mission_data
         self.connected = False
 
     def handleMessage(self):
-      if self.connected:
-         data = '{"obstacle_present":' + self.vehicle_state_data[0].get_obstacle_in_path() +  ', '
-         data += '"0": ' + str(self.mission_data[0]).encode("utf-8") + ', '
-         data += '"alt": ' + str(self.vehicle_state_data[0].get_location().get_alt())
-         data += ', "dir": ' + str(self.vehicle_state_data[0].get_direction())
-         data += ', "speed": ' + str(self.vehicle_state_data[0].get_groundspeed())
-         data += ', "velocity": ' + str(self.vehicle_state_data[0].get_velocity())
-         data += ', "lat": ' + str(self.vehicle_state_data[0].get_location().get_lat()) + ', "long": ' + str(self.vehicle_state_data[0].get_location().get_lon()) + "}"
+        if self.connected:
+            data = '{"obstacle_present":' + str(self.vehicle_state_data[0].get_obstacle_in_path()).lower() +  ', '
+            data += '"0": ' + str(self.mission_data[0]).encode("utf-8") + ', '
+            data += '"alt": ' + str(self.vehicle_state_data[0].get_alt())
+            data += ', "dir": ' + str(self.vehicle_state_data[0].get_direction())
+            data += ', "speed": ' + str(self.vehicle_state_data[0].get_groundspeed())
+            data += ', "velocity": ' + str(self.vehicle_state_data[0].get_velocity())
+            data += ', "lat": ' + str(self.vehicle_state_data[0].get_lat()) + ', "long": ' + str(self.vehicle_state_data[0].get_lon()) + "}"
+            data = data.replace("\'", "\"")
 
-         client.sendMessage(data.encode("utf-8"))
+            self.sendMessage(str(data).encode("utf-8"))
 
     def handleConnected(self):
        print(self.address, 'connected')
