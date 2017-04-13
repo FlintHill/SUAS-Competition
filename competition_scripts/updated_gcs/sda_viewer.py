f94f8d7ec87daa667902f0703c65f9b35b20910c
Vale <valetolpegin@gmail.com>
2017-04-13 13:42:30 -0400

    Update SDA Viewer web socket to work with new changes

--- competition_scripts/updated_gcs/sda_viewer.py
+++ competition_scripts/updated_gcs/sda_viewer.py
@@ -2,26 +2,27 @@ from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
 
 class SDAViewSocket(WebSocket):
 
-    def __init__(self, p1, p2, p3, vehicle_state_data, obstacle_data):
+    def __init__(self, p1, p2, p3, vehicle_state_data, mission_data):
         """
         Initialize
         """
         super(self, SDAViewSocket).__init__(p1, p2, p3)
 
         self.vehicle_state_data = vehicle_state_data
-        self.obstacle_data = obstacle_data
+        self.mission_data = mission_data
         self.connected = False
 
     def handleMessage(self):
       if self.connected:
          data = '{"obstacle_present":' + self.vehicle_state_data[0].get_obstacle_in_path() +  ', '
+         data += '"0": ' + str(self.mission_data[0]).encode("utf-8") + ', '
          data += '"alt": ' + str(self.vehicle_state_data[0].get_location().get_alt())
          data += ', "dir": ' + str(self.vehicle_state_data[0].get_direction())
          data += ', "speed": ' + str(self.vehicle_state_data[0].get_groundspeed())
          data += ', "velocity": ' + str(self.vehicle_state_data[0].get_velocity())
          data += ', "lat": ' + str(self.vehicle_state_data[0].get_location().get_lat()) + ', "long": ' + str(self.vehicle_state_data[0].get_location().get_lon()) + "}"
 
-         client.sendMessage(unicode(data))
+         client.sendMessage(data.encode("utf-8"))
 
     def handleConnected(self):
        print(self.address, 'connected')
