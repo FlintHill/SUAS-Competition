dc4ac3039279453965165f1b814cec3597bebab0
Vale <valetolpegin@gmail.com>
2017-09-11 14:46:34 -0400

    Update install script to be more comprehensive

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 # Update all packages
 
+git clone https://github.com/auvsi-suas/interop
+cd interop/client
+sudo -H python -m pip install -r requirements.txt
+sudo -H python -m pip install . -U
+cd ../../
+sudo rm -r interop
+
 sudo -H python -m pip install ./EigenFit -U
 sudo -H python -m pip install ./ImgProcessingCLI -U
 sudo -H python -m pip install ./SUASSystem -U
