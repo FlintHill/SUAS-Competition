551e1554e8c69e550443f734cbc3229ed4b6cd8c
nspivy <nspivy@flinthill.org>
2017-09-14 14:22:24 -0400

    Created Unit Test for SyntheticDataset.ColorMath

---
+++ tests/test_color_math.py
@@ -0,0 +1,22 @@
+from SyntheticDataset import *
+import unittest
+from math import sqrt
+
+class TestColorMath(unittest.TestCase):
+
+    def test_get_magnitude_between_colors(self):
+        colorA = [0,0,220]
+        colorB = [10,80,225]
+        magnitude = getMagnitudeBetweenColors(colorA, colorB)
+        self.assertEqual(sqrt(6525), magnitude)
+
+    def test_get_most_similar_color(self):
+        color = [204,0,0]
+        red = [255,0,0]
+        blue = [0,0,255]
+        yellow = [255,255,0]
+        green = [0,255,0]
+        purple = [255,0,255]
+        colors = [red,blue,yellow,green,purple]
+        similarColor = getMostSimilarColor(colors, color)
+        self.assertEqual(red, similarColor)
