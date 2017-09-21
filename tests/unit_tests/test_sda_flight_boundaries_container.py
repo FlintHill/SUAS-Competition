e5a07f0b9d026671a46f57fc88e92a7e01765b20
Your Name <zyin@flinthill.org>
2017-09-21 11:49:33 -0400

    Completed flight_boundary and flight_boundareis_container

--- tests/unit_tests/test_sda_flight_boundaries_container.py
+++ tests/unit_tests/test_sda_flight_boundaries_container.py
@@ -6,11 +6,13 @@ class FlightBoundariesContainerTestCase(unittest.TestCase):
 
     def setUp(self):
 
+        fly_zones = np.array([np.array([(0,0),(0,100),(100,100),(100,0)]),np.array([(-200,0),(-200,-200),(0,-200),(0,0)])])
         my_fly_zones = np.array([FlightBoundary(Constants.MIN_ALT, Constants.MAX_ALT, boundary_points) for boundary_points in fly_zones])
-        self.my_flight_boundaries_container = FlightBoundariesContainer(my_fly_zones)
+        self.my_flight_boundaries_container = FlightBoundariesContainer(fly_zones)
 
     def test_is_point_in_bounds(self):
 
-        my_point = np.array([20, 30])
-
-        self.assertTrue(FlightBoundariesContainer.is_point_in_bounds(my_point))
+        my_point1 = np.array([20, 30, 40])
+        self.assertTrue(self.my_flight_boundaries_container.is_point_in_bounds(my_point1), True)
+        my_point2 = np.array([1000, 1000, 1000])
+        self.assertFalse(self.my_flight_boundaries_container.is_point_in_bounds(my_point2), True)
--- tests/unit_tests/test_sda_flight_boundary.py
+++ tests/unit_tests/test_sda_flight_boundary.py
@@ -9,8 +9,9 @@ class FlightBoundaryTestCase(unittest.TestCase):
 
         my_min_altitude = 50.0
         my_max_altitude = 200.0
-        my_bound_path = np.array([(0,0),(0,100),(100,100),(100,0)])
-        self.my_flight_boundary = FlightBoundary(my_min_altitude, my_max_altitude, my_bound_path)
+        my_boundary_waypoints = np.array([(0,0),(0,100),(100,100),(100,0)])
+        my_bound_path = Path(my_boundary_waypoints)
+        self.my_flight_boundary = FlightBoundary(my_min_altitude, my_max_altitude, my_boundary_waypoints)
 
     def test_is_point_in_bounds(self):
 
@@ -18,24 +19,7 @@ class FlightBoundaryTestCase(unittest.TestCase):
         print self.my_flight_boundary.is_point_in_bounds(my_point)
         self.assertTrue(self.my_flight_boundary.is_point_in_bounds(my_point), True)
 
-    def is_point_in_bounds(self, point):
-        """
-        Returns True if the passed point is in the boundaries, False if not
-
-        :param point: The point to test
-        :type point: Numpy Array
-        """
-        dim_reduced_point = point[:2]
-        is_point_in_polygon = self.path_method(dim_reduced_point)
-        is_point_in_alts = (point[2] > self.min_altitude and point[2] < self.max_altitude)
-
-        if is_point_in_polygon and is_point_in_alts:
-            return True
-
-        return False
-
-
     def test_path_method(self):
-        my_bound_path = Path([(0,0),(0,100),(100,100),(100,0)])
-        my_point = np.array([[50, 50]])
-        self.assertTrue(my_bound_path.contains_points([my_point]), True)
+
+        my_point = np.array([50, 50])
+        self.assertTrue(self.my_flight_boundary.path_method(my_point), True)
