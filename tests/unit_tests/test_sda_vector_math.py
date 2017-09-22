e60be9172c691447897d7ef68019bfca994801c7
Your Name <zyin@flinthill.org>
2017-09-21 20:20:20 -0400

    Completed Test for Vector Math. Suggesting to round the results of Vector Math

--- tests/unit_tests/test_sda_vector_math.py
+++ tests/unit_tests/test_sda_vector_math.py
@@ -10,17 +10,17 @@ class VectorMathTestCase(unittest.TestCase):
 
     def test_get_magnitude(self):
 
-        my_vector_one = np.array([2.0, 3.0, 3.0])
-        my_vector_two = np.array([6.0, 6.0, 6.0])
+        my_vector1 = np.array([2.0, 3.0, 3.0])
+        my_vector2 = np.array([6.0, 6.0, 6.0])
 
-        self.assertEqual(VectorMath.get_magnitude(my_vector_one, my_vector_two), math.sqrt(34.0))
+        self.assertEqual(VectorMath.get_magnitude(my_vector1, my_vector2), math.sqrt(34.0))
 
     def test_get_unit_vector_from_two_vectors(self):
 
-        my_vector_one = np.array([2.0, 3.0, 3.0])
-        my_vector_two = np.array([6.0, 6.0, 6.0])
+        my_vector1 = np.array([2.0, 3.0, 3.0])
+        my_vector2 = np.array([6.0, 6.0, 6.0])
 
-        self.assertTrue(np.array_equal(VectorMath.get_unit_vector_from_two_vectors(my_vector_one, my_vector_two), np.array([4.0/math.sqrt(34.0), 3.0/math.sqrt(34.0), 3.0/math.sqrt(34.0)])))
+        self.assertTrue(np.array_equal(VectorMath.get_unit_vector_from_two_vectors(my_vector1, my_vector2), np.array([4.0/math.sqrt(34.0), 3.0/math.sqrt(34.0), 3.0/math.sqrt(34.0)])))
 
     def test_get_single_unit_vector(self):
 
@@ -35,32 +35,39 @@ class VectorMathTestCase(unittest.TestCase):
         self.assertEqual(VectorMath.get_vector_magnitude(my_vector), math.sqrt(22.0))
 
     def test_get_vector_projection(self):
-        """
-        my_vector_one = np.array([2.0, 3.0, 3.0])
-        my_vector_two = np.array([6.0, 6.0, 6.0])
-        """
 
-        my_vector_three = np.array([-33.0, 0, 66.5])
-        my_vector_four = np.array([0, 88.4, -32.6])
+        my_vector1 = np.array([2, 3, 3])
+        my_vector2 = np.array([6, 6, 6])
 
-        print(VectorMath.get_vector_projection(my_vector_three, my_vector_four))
-        """
-        self.assertTrue(np.array_equal(VectorMath.get_vector_projection(my_vector_one, my_vector_two), np.array([8.0/3.0, 8.0/3.0, 8.0/3.0])))
-        """
-        self.assertTrue(np.array_equal(VectorMath.get_vector_projection(my_vector_three, my_vector_four), np.array([-0., -21.5878621, 7.96113467])))
+        my_vector3 = np.array([-33.0, 0, 66.5])
+        my_vector4 = np.array([0, 88.4, -32.6])
 
-        vector_five
+        my_vector5 = np.array([1,2,3])
+        my_vector6 = np.array([-3,-2,-1])
+
+        print(VectorMath.get_vector_projection(my_vector1, my_vector2))
+        print(VectorMath.get_vector_projection(my_vector3, my_vector4))
+        print(VectorMath.get_vector_projection(my_vector5, my_vector6))
+
+        self.assertTrue(np.array_equal(np.round(VectorMath.get_vector_projection(my_vector1, my_vector2), decimals = 5, out = None), np.round(np.array([8.0/3.0, 8.0/3.0, 8.0/3.0]), decimals = 5, out = None)))
+        self.assertTrue(np.array_equal(np.round(VectorMath.get_vector_projection(my_vector3, my_vector4), decimals = 5, out = None), np.round(np.array([-0., -21.5878621, 7.96113467]), decimals = 5, out = None)))
+        self.assertTrue(np.array_equal(np.round(VectorMath.get_vector_projection(my_vector5, my_vector6), decimals = 5, out = None), np.round(np.array([15.0/7.0, 10.0/7.0, 5.0/7.0]), decimals = 5, out = None)))
 
     def test_get_vector_rejection(self):
-        """
-        my_vector_one = np.array([2.0, 3.0, 3.0])
-        my_vector_two = np.array([6.0, 6.0, 6.0])
-        """
-        my_vector_three = np.array([-33.0, 0, 66.5])
-        my_vector_four = np.array([0, 88.4, -32.6])
 
-        print(VectorMath.get_vector_rejection(my_vector_three, my_vector_four))
-        """
-        self.assertTrue(np.array_equal(VectorMath.get_vector_rejection(my_vector_one, my_vector_two), np.array([-2.0/3.0, 1.0/3.0, 1.0/3.0])))
-        """
-        self.assertTrue(np.array_equal(VectorMath.get_vector_rejection(my_vector_three, my_vector_four), np.array([-33., 21.5878621, 58.53886533])))
+        my_vector1 = np.array([2, 3, 3])
+        my_vector2 = np.array([6, 6, 6])
+
+        my_vector3 = np.array([-33.0, 0, 66.5])
+        my_vector4 = np.array([0, 88.4, -32.6])
+
+        my_vector5 = np.array([1,2,3])
+        my_vector6 = np.array([-3,-2,-1])
+
+        print(VectorMath.get_vector_rejection(my_vector1, my_vector2))
+        print(VectorMath.get_vector_rejection(my_vector3, my_vector4))
+        print(VectorMath.get_vector_rejection(my_vector5, my_vector6))
+
+        self.assertTrue(np.array_equal(np.round(VectorMath.get_vector_rejection(my_vector1, my_vector2), decimals = 5, out = None), np.round(np.array([-2.0/3.0, 1.0/3.0, 1.0/3.0]), decimals = 5, out = None)))
+        self.assertTrue(np.array_equal(np.round(VectorMath.get_vector_rejection(my_vector3, my_vector4), decimals = 5, out = None), np.round(np.array([-33, 21.5878621, 58.53886533]), decimals = 5, out = None)))
+        self.assertTrue(np.array_equal(np.round(VectorMath.get_vector_rejection(my_vector5, my_vector6), decimals = 5, out = None), np.round(np.array([-8.0/7.0, 4.0/7.0, 16.0/7.0]), decimals = 5, out = None)))
