c7da1663e2a86546ca250af0a86ea0aee02a1828
nspivy <nspivy@flinthill.org>
2017-10-01 10:02:15 -0400

    kernel applier done

--- tests/unit_tests/test_color_math.py
+++ tests/unit_tests/test_color_math.py
@@ -7,7 +7,7 @@ class TestColorMath(unittest.TestCase):
     def test_get_magnitude_between_colors(self):
         colorA = [0,0,220]
         colorB = [10,80,225]
-        magnitude = getMagnitudeBetweenColors(colorA, colorB)
+        magnitude = ColorMath.getMagnitudeBetweenColors(colorA, colorB)
         self.assertEqual(sqrt(6525), magnitude)
 
     def test_get_most_similar_color(self):
@@ -18,5 +18,5 @@ class TestColorMath(unittest.TestCase):
         green = [0,255,0]
         purple = [255,0,255]
         colors = [red,blue,yellow,green,purple]
-        similarColor = getMostSimilarColor(colors, color)
+        similarColor = ColorMath.getMostSimilarColor(colors, color)
         self.assertEqual(red, similarColor)
--- tests/unit_tests/test_compass_angler.py
+++ tests/unit_tests/test_compass_angler.py
@@ -9,14 +9,12 @@ class TestCompassAngler(unittest.TestCase):
         self.angle2 = 5
         self.angle3 = 0.4
         comp_angle1 = CompassAngler.getCompassAnglesSortedByProximityToAngle(self.angle1)
-        comp_angle2 = CompassAngler.getCompassAnglesSortedByProximityToAngle(angle2)
-        comp_angle3 = CompassAngler.getCompassAnglesSortedByProximityToAngle(angle3)
-        self.assertEqual([("W", math.pi), ("NW", 3.0*math.pi/4.0), ("N", math.pi/2.0), ("NE", math.pi/4.0), ("E", 0), ("SE", 7.0*math.pi/4.0), ("S", 3.0*math.pi/2.0), ("SW", 5.0*math.pi/4.0)], comp_angle1)
-        self.assertEqual([("E", 0), ("SE", 7.0*math.pi/4.0), ("S", 3.0*math.pi/2.0), ("SW", 5.0*math.pi/4.0), ("W", math.pi), ("NW", 3.0*math.pi/4.0), ("N", math.pi/2.0), ("NE", math.pi/4.0)], comp_angle2)
-        self.assertEqual([("N", math.pi/2.0), ("NE", math.pi/4.0), ("E", 0), ("SE", 7.0*math.pi/4.0), ("S", 3.0*math.pi/2.0), ("SW", 5.0*math.pi/4.0), ("W", math.pi), ("NW", 3.0*math.pi/4.0)], comp_angle3)
-
-        #the + math.pi/2 means it starts at north
-        #the code works correctly
+        comp_angle2 = CompassAngler.getCompassAnglesSortedByProximityToAngle(self.angle2)
+        comp_angle3 = CompassAngler.getCompassAnglesSortedByProximityToAngle(self.angle3)
+        self.assertEqual([("SW", 5.0*math.pi/4.0), ("W", math.pi), ("S", 3.0*math.pi/2.0), ("NW", 3.0*math.pi/4.0), ("SE", 7.0*math.pi/4.0), ("N", math.pi/2.0), ("E", 2*math.pi), ("NE", math.pi/4.0), ("E", 0)], comp_angle1)
+        self.assertEqual([("E", 0), ("NE", math.pi/4.0), ("N", math.pi/2.0), ("NW", 3.0*math.pi/4.0), ("W", math.pi), ("SW", 5.0*math.pi/4.0), ("S", 3.0*math.pi/2.0), ("SE", 7.0*math.pi/4.0), ("E", 2.0*math.pi)], comp_angle2)
+        self.assertEqual([("NW", 3.0*math.pi/4.0), ("N", math.pi/2.0), ("W", math.pi), ("NE", math.pi/4.0), ("SW", 5.0*math.pi/4.0), ("E", 0), ("S", 3.0*math.pi/2.0), ("SE", 7.0*math.pi/4.0), ("E", 2.0*math.pi)], comp_angle3)
+    
     def test_get_closest_compass_angle(self):
         angle1 = 2
         angle2 = 5
@@ -24,9 +22,9 @@ class TestCompassAngler(unittest.TestCase):
         comp_angle1 = CompassAngler.getClosestCompassAngle(angle1)
         comp_angle2 = CompassAngler.getClosestCompassAngle(angle2)
         comp_angle3 = CompassAngler.getClosestCompassAngle(angle3)
-        self.assertEqual([("W", math.pi)], comp_angle1)
-        self.assertEqual([("E", 0)], comp_angle2)
-        self.assertEqual([("N", math.pi/2.0)], comp_angle3)
+        self.assertEqual(("SW"), comp_angle1)
+        self.assertEqual(("E"), comp_angle2)
+        self.assertEqual(("NW"), comp_angle3)
 
     def test_get_random_compass_angle(self):
         num = CompassAngler.getRandomCompassAngle()
