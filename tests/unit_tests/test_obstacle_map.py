8d1e59860d4d2368f047716a72a1d7c24a7ce805
BrightStraightReality <rod.thinking@gmail.com>
2017-10-16 17:09:11 -0400

    Fixed basic errors found in unit testing

--- tests/unit_tests/test_obstacle_map.py
+++ tests/unit_tests/test_obstacle_map.py
@@ -1,9 +1,9 @@
 from SDA import *
 import unittest
-import numpy 
+import numpy
 
 class Test_Obstacle_map(unittest.TestCase):
-	
+
 	def setUp(self):
 		self.test_drone_point = numpy.array([200,200,2])
 		self.test_fly_zones = numpy.array([numpy.array([(0,0), (0,300), (300,300), (300,0)])])
@@ -17,13 +17,16 @@ class Test_Obstacle_map(unittest.TestCase):
 		self.obstacle_map3 = ObstacleMap(self.test_drone_point3,self.test_fly_zones2)
 
 	def test_add_obstacle(self):
-		"""	
+		"""
 		 test if the last obstacle in obstacle_map is the added object
 		"""
-		
+
 		self.test_obstacle = StationaryObstacle(numpy.array([1,1,1]),2,1)
 		self.obstacle_map1.add_obstacle(self.test_obstacle)
-		
+
+		#self.test_obstacle2 = StationaryObstacle(numpy.array([1,1,1]),4,1)
+		#self.test_obstacle3 = StationaryObstacle(numpy.array([1,1,1]),2,1)
+
 		self.assertEqual(self.obstacle_map1.get_obstacles(),self.test_obstacle)
 
 	def test_add_waypoint(self):
@@ -32,9 +35,9 @@ class Test_Obstacle_map(unittest.TestCase):
 		"""
 
 		self.new_waypoint = numpy.array([1,1,1])
-		
+
 		self.obstacle_map1.add_waypoint(self.new_waypoint)
-		
+
 		self.added_waypoint = self.obstacle_map1.get_drone().get_waypoint_holder().get_current_waypoint()
 
 		self.assertEqual(self.added_waypoint.all(),self.new_waypoint.all())
@@ -93,7 +96,7 @@ class Test_Obstacle_map(unittest.TestCase):
 			self.assertTrue(not self.obstacle_map1.does_path_intersect_obstacle_3d(self.test_obstacle,self.attempt_paths[0][0],self.attempt_paths[0][1]))
 			print (paths)
 
-	
+
 	def test_get_obstacles(self):
 		self.obstacle_map1.reset_obstacles()
 		self.test_obstacle = StationaryObstacle(numpy.array([150,150,2]),10,300)
@@ -131,4 +134,3 @@ class Test_Obstacle_map(unittest.TestCase):
 		self.drone_point = numpy.array([150,150,2])
 		self.test_waypoint = numpy.array([180,180,2])
 		self.assertTrue(self.obstacle_map1.does_path_intersect_obstacle_3d(self.test_obstacle,self.drone_point,self.test_waypoint))
-
