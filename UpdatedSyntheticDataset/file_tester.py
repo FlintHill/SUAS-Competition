c2a5bf6a846812130929ad04530a72089620cdee
Your Name <zyin@flinthill.org>
2017-10-25 10:50:01 -0400

    Changes in file_tester

--- UpdatedSyntheticDataset/file_tester.py
+++ UpdatedSyntheticDataset/file_tester.py
@@ -18,6 +18,23 @@ ImageResizer.resize_image_free(image1, 30.0).show()
 
 image2 = SpecifiedTargetCreator.create_specified_target("circle", "N", "Z", 1000, 2, (255, 255, 255, 255), (255, 0, 0, 255), 45)
 ImageResizer.resize_image_free(image2, 30.0).show()
-"""
+
 image3 = TargetWithBackgroundCreator.create_random_target_with_random_background([500, 1000], [1.5, 2.5], "/Users/zyin/Desktop/Competition Backgrounds", 0, 2)
 image3.show()
+"""
+
+image1 = SpecifiedTargetCreator.create_specified_target("star", "N", "W", 500, 1.5, (255, 255, 255, 255), (255, 0, 0, 255), 0)
+image2 = SpecifiedTargetCreator.create_specified_target("star", "N", "W", 500, 2.0, (255, 255, 255, 255), (255, 0, 0, 255), 0)
+image3 = SpecifiedTargetCreator.create_specified_target("star", "N", "W", 500, 2.5, (255, 255, 255, 255), (255, 0, 0, 255), 0)
+
+image4 = SpecifiedTargetCreator.create_specified_target("quarter_circle", "N", "W", 500, 1.5, (255, 255, 255, 255), (255, 0, 0, 255), 0)
+image5 = SpecifiedTargetCreator.create_specified_target("quarter_circle", "N", "W", 500, 2.0, (255, 255, 255, 255), (255, 0, 0, 255), 0)
+image6 = SpecifiedTargetCreator.create_specified_target("quarter_circle", "N", "W", 500, 2.5, (255, 255, 255, 255), (255, 0, 0, 255), 0)
+
+image1.show()
+image2.show()
+image3.show()
+
+image4.show()
+image5.show()
+image6.show()
