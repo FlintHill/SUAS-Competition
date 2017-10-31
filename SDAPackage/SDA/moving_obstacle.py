eb5291e28afa0547bba4c019f678865f76c52ae7
Yudong Li <yudongli@s17usw8usisian2.flinthill.org>
2017-10-31 12:40:13 -0400

    revision for moving obstacle

--- SDAPackage/SDA/moving_obstacle.py
+++ SDAPackage/SDA/moving_obstacle.py
@@ -2,25 +2,25 @@ from Obstacle import *
 from SDA import Constants
 
 class MovingObstacle(Obstacle)
+	"""
+	sample
+	moving_obstacles": [
+        {
+            "altitude_msl": 189.56748784643966,
+            "latitude": 38.141826869853645,
+            "longitude": -76.43199876559223,
+            "sphere_radius": 150.0
+        }
+	"""
 	
 
-	def __init__(self, point, radius, height, velocity)
-		super(MovingObstacle, self).__init__(point, Constants.STATIONARY_OBSTACLE_SAFETY_RADIUS)
+	def __init__(self, point, radius):
+		super(MovingObstacle, self).__init__(point, Constants.MOVING_OBSTACLE_SAFETY_RADIUS)
 
 		self.radius = radius
-		self.height = height
 		self.velocity = velocity
 
-	def get_radius(self)
+	def get_radius(self):
 
-		return self.radius
-
-	def get_height(self)
-
-		return self.height
-
-	def get_velocity(self)
-
-		return self.velocity
-
+		return self.radius+self.get_safety_radius()
 	
