c5de20ea50e99d151f9108ecc08c974299b2ee69
Yudong Li <yudongli@s17usw8usisian2.flinthill.org>
2017-11-01 08:00:03 -0400

    spacing

--- tests/my_tests/test_moving_obstacle.py
+++ tests/my_tests/test_moving_obstacle.py
@@ -2,14 +2,14 @@ from SDA import *
 import unittest
 import numpy
 
-class  MovingObstacleTestCase(object):
+class MovingObstacleTestCase(object):
 	"""docstring for  MovingObstacleTestCase"""
 	def setUp(self):
-		self.point = numpy.array([0,0,0])
-		self.radius = 5
-		self.MovingObstacle = MovingObstacle(point, radius)
+		  self.point = numpy.array([0,0,0])
+		  self.radius = 5
+		  self.MovingObstacle = MovingObstacle(point, radius)
 
 	def test_get_radius(self):
-		self.assrtEqual(35, MovingObstacle.get_radius())
+		  self.assrtEqual(35, MovingObstacle.get_radius())
 
 
