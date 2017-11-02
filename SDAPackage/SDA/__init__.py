b7808558af776a48d38985516918e0ff750a1fa1
Yudong Li <yudongli@s17usw8usisian2.flinthill.org>
2017-11-02 11:37:43 -0400

    added moving obstacle class into __init__ fixed class and unittest

--- SDAPackage/SDA/__init__.py
+++ SDAPackage/SDA/__init__.py
@@ -2,6 +2,7 @@ from .constants import Constants
 from .obstacle import Obstacle
 from .vector_math import VectorMath
 from .waypoint_holder import WaypointHolder
+from .moving_obstacle import MovingObstacle
 from .stationary_obstacle import StationaryObstacle
 from .drone import Drone
 from .flight_boundary import FlightBoundary
