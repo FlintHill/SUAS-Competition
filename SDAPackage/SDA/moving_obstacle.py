876cc61ac80de41c0e03f42d60515c7e6581ea78
Yudong Li <yudongli@s17usw8usisian2.flinthill.org>
2017-11-02 08:05:44 -0400

    fixed indentation errors

--- SDAPackage/SDA/moving_obstacle.py
+++ SDAPackage/SDA/moving_obstacle.py
@@ -2,9 +2,9 @@ from SDA import Obstacle
 from SDA import Constants
 
 class MovingObstacle(Obstacle):
-	"""
-	sample
-	moving_obstacles": [
+    """
+    sample
+    moving_obstacles": [
         {
             "altitude_msl": 189.56748784643966,
             "latitude": 38.141826869853645,
@@ -12,22 +12,22 @@ class MovingObstacle(Obstacle):
             "sphere_radius": 150.0
         }
         ]
-	"""
+    """
 
     def __init__(self, point, radius):
-		"""
-		:param point: The point for the stationary obstacle
+        """
+        :param point: The point for the stationary obstacle
         :type point: Numpy Array
         :param radius: The radius of the obstacle
         :type radius: Float
         """
-		super(MovingObstacle, self).__init__(point, Constants.MOVING_OBSTACLE_SAFETY_RADIUS)
+        super(MovingObstacle, self).__init__(point, Constants.MOVING_OBSTACLE_SAFETY_RADIUS)
 
-		self.radius = radius
+        self.radius = radius
 
     def get_radius(self):
-		"""
+        """
         Return the radius of the obstacle plus that of the safety radius
         """
-		return self.radius + self.get_safety_radius()
-	
+        return self.radius + self.get_safety_radius()
+    
