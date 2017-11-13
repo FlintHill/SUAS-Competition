10cde748edcd522c20efdeee8d080873b977117e
NSpivy <nspivy@flinthill.org>
2017-11-13 10:56:19 -0500

    Update startup_picture_serve_and_take.sh

--- scripts/gcs/a6000/startup_picture_serve_and_take.sh
+++ scripts/gcs/a6000/startup_picture_serve_and_take.sh
@@ -1,9 +1,7 @@
 #!/bin/sh -
 
-A6000_PATH=/home/odroid/Desktop/SUAS-Competition/scripts/gcs/a6000
-
 cd /home/odroid/Desktop/SUAS-Competition/scripts/gcs/a6000
 
-python $A6000_PATH/picture_server.py &
+python picture_server.py &
 sleep 10
-bash $A6000_PATH/picture_taker.sh
+bash picture_taker.sh
