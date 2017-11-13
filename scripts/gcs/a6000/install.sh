42a5ac0ffcadd53837197cff4e97cb294dbe182e
nspivy <nspivy@flinthill.org>
2017-11-13 11:22:48 -0500

    removed repitition and fixed pip download in install.sh

--- scripts/gcs/a6000/install.sh
+++ scripts/gcs/a6000/install.sh
@@ -3,19 +3,12 @@ apt-get update
 apt-get upgrade -y
 apt-get dist-upgrade -y
 
-apt-get update
-apt-get upgrade -y
-apt-get dist-upgrade -y
-
 # get apps
 apt-get install git -y
 
 # get pip
 cd ~/Downloads/
-
-wget https://bootstrap.pypa.io/get-pip.py
-python get-pip.py
-rm get-pip.py
+sudo apt-get install python3-pip
 
 # install gphoto
 wget https://raw.githubusercontent.com/gonzalo/gphoto2-updater/master/gphoto2-updater.sh && chmod +x gphoto2-updater.sh && sudo ./gphoto2-updater.sh
