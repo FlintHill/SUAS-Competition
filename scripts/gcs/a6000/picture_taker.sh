a15de8df01b297bc44725ed09cf67ff1d279f31e
NSpivy <nspivy@flinthill.org>
2017-11-13 10:53:25 -0500

    Update picture_taker.sh

--- scripts/gcs/a6000/picture_taker.sh
+++ scripts/gcs/a6000/picture_taker.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+waitTime = 5
+
 while [ true ]
 do
   fn=$(date +%s)
   gphoto2 --capture-image-and-download --filename image-$fn.jpg
   mv image-$fn.jpg imgs/
-  sleep 5
+  sleep $waitTime
 done
