81046f1b5c1f01378325cc0de8dff686de0c2f4f
nspivy <nspivy@flinthill.org>
2017-11-28 14:57:56 -0500

    I made extensive changes to the photo_downloader

--- scripts/gcs/a6000/photo_downloader.py
+++ scripts/gcs/a6000/photo_downloader.py
@@ -1,21 +1,27 @@
-import calendar
-import time
 import requests
+import json
+import os
 import shutil
 
-number = 0
-while true:
 
-    url1 = 'http://odroid@INPUT_PORT/get/imgs'
-    response = requests.get(url1)
-    list = response.content
-    if (str(number) == list[list.index(str(number):list.index(str(number)+1]):
-        imgName = list[list.index(number)+5:list.index(number)+19]
-        url2 = 'http://odroid@INPUT_PORT/get/imgs/' + imgName
-        response = requests.get(url2, stream=True)
-        with open('img-'+number+ '.png', 'wb') as out_file:
+while True:
+    r = requests.get("http://0.0.0.0:5000/get/imgs")
+    imgs = json.load(r.text)
+
+    downloaded_photos = []
+
+    for(dirpath, dirnames, filenames) in walk(img):
+        downloaded_photos = filenames
+
+    photos_to_download = []
+
+    for i in range(0, len(imgs)):
+        photo_to_download = list(set(imgs) - set(downloaded_photos))
+
+    for image in photos_to_download:
+        response = requests.get('http://odroid@INPUT_PORT/get/imgs/' + image, stream=True)
+
+        with open(image, 'wb') as out_file:
             shutil.copyfileobj(response.raw, out_file)
-        del response
-        number = number + 1
 
-    sleep 4
+        del response
