09499fe9c7e0228896572146045491ef18681df2
jmoxrox <jmoxrox@gmail.com>
2017-11-30 14:27:28 -0500

    Added changes for other drone types in image processing

--- SUASSystem/SUASSystem/utils/image_operations.py
+++ SUASSystem/SUASSystem/utils/image_operations.py
@@ -1,9 +1,19 @@
-import math
+import math, exifread
+from datetime import datetime
 from SUASSystem import GCSSettings, inverse_haversine
 
-def get_image_timestamp(filename):
+def get_image_timestamp_from_filename(filename):
     filename = filename[6:]
     return float(filename[:-4])
+
+def get_image_timestamp_from_metadata(path_to_image):
+    image = open(path_to_image, "rb")
+    tags = exifread.process_file(image, details=False)
+    image_raw_time = tags["Image DateTime"]
+    image_date_time = datetime.strptime(str(image_raw_time), "%Y:%m:%d %H:%M:%S")
+    image_epoch_time = (image_date_time-datetime(1970,1,1)).total_seconds()
+    return image_epoch_time
+
 
 def get_target_gps_location(image_midpoint, target_midpoint, drone_gps_location):
     difference_x = target_midpoint[0] - image_midpoint[0]
