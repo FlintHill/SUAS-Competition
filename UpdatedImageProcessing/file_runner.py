cf24ff9e08cc95b4464b1009faba877aa639221f
Your Name <zyin@flinthill.org>
2018-01-10 19:01:39 -0500

    Separated the detection process from the testing process to prepare for the integration

--- UpdatedImageProcessing/file_runner.py
+++ UpdatedImageProcessing/file_runner.py
@@ -1,6 +1,12 @@
+<<<<<<< HEAD
 import os
 import timeit
 import json
+||||||| merged common ancestors
+=======
+import os
+import timeit
+>>>>>>> Separated the detection process from the testing process to prepare for the integration
 from ImageProcessing2.TargetDetection import *
 from ImageProcessing2.TargetDetectionTester import *
 
@@ -9,7 +15,15 @@ start_time = timeit.default_timer()
 #Run UpdatedImageProcessing here. More parameters can be changed in target_detection_settings.py.
 
 #Detect targets on all target maps in the given path
+<<<<<<< HEAD
+||||||| merged common ancestors
+MassTargetDetector.detect_mass_target()
+=======
 
+combo_target_detection_result_list = MassTargetDetector.detect_mass_target(os.path.expanduser("~/Desktop/Synthetic_Dataset/Answers/modular_target_maps"))
+>>>>>>> Separated the detection process from the testing process to prepare for the integration
+
+<<<<<<< HEAD
 combo_target_detection_result_list = MassTargetDetector.detect_mass_target(os.path.expanduser("~/Desktop/Synthetic_Dataset/Answers/modular_target_maps"))
 
 Logger.format_time_report(timeit.default_timer() - start_time)
@@ -17,3 +31,12 @@ Logger.format_time_report(timeit.default_timer() - start_time)
 TargetDetectionResultSaver.save_target_detection_result(combo_target_detection_result_list)
 
 AutomaticTester.run_automatic_tester()
+||||||| merged common ancestors
+TargetDetectionLogger.format_time_report(timeit.default_timer() - start_time)
+=======
+TargetDetectionLogger.format_time_report(timeit.default_timer() - start_time)
+
+TargetDetectionResultSaver.save_target_detection_result(combo_target_detection_result_list)
+
+AutomaticTester.run_automatic_tester()
+>>>>>>> Separated the detection process from the testing process to prepare for the integration
