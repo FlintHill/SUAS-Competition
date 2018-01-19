c5d1d07256b63850c561bacb3020867fcdcef371
Your Name <zyin@flinthill.org>
2018-01-18 19:11:18 -0500

    Trying hard to fix importing problems.......

--- install.sh
+++ install.sh
@@ -15,6 +15,7 @@ sudo rm -r simple-websocket-server
 
 sudo -H python -m pip install opencv-python
 sudo -H python -m pip install scikit-learn
+sudo -H python -m pip install scipy
 
 sudo -H python -m pip install dronekit
 sudo -H python -m pip install wxpython
