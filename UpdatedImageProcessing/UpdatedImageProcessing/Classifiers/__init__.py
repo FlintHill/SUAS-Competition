8eba05c88bec081273448338ef669aa885190e74
Your Name <zyin@flinthill.org>
2018-01-29 19:05:15 -0500

    Rebased and reorganized files

--- UpdatedImageProcessing/UpdatedImageProcessing/Classifiers/__init__.py
+++
@@ -1,4 +0,0 @@
-from .settings import ImageProcessingClassifierSettings
-from .logger import Logger
-
-from .classify_color import ColorClassifier
--- UpdatedImageProcessing/UpdatedImageProcessing/Classifiers/logger.py
+++
@@ -1,8 +0,0 @@
-from .settings import ImageProcessingClassifierSettings
-
-class Logger(object):
-
-    @staticmethod
-    def log(message):
-        if ImageProcessingClassifierSettings.LOGGING_ON:
-            print(message)
