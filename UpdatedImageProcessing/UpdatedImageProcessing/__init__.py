cf6d41b5d49bc2f3838dacc2237800358972f11e
Vale <valetolpegin@gmail.com>
2018-03-03 11:24:11 -0500

    Remove reference to old folder

--- UpdatedImageProcessing/UpdatedImageProcessing/__init__.py
+++ UpdatedImageProcessing/UpdatedImageProcessing/__init__.py
@@ -1,7 +1,6 @@
 from .ShapeDetection import *
 from .TargetDetection import *
 from .TargetDetectionTester import *
-from .ColorDetection import *
 from .Classifiers import *
 from .integrated_image_processing import IntegratedImageProcessing
 from .IntegratedImageProcessingTester import *
