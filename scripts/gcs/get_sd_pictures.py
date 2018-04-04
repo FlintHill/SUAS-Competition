f51c2024e045f4ea9f83ffe7e8ec9e87d44a9911
jmoxrox <jmoxrox@gmail.com>
2018-04-04 11:01:39 -0400

    patch for shape classification and update for how get_sd_pictures

--- scripts/gcs/get_sd_pictures.py
+++ scripts/gcs/get_sd_pictures.py
@@ -16,11 +16,11 @@ if __name__ == '__main__':
 
     print("SD Card loaded")
 
-    os.system("rm static/imgs/*.JPG")
+    os.system("rm static/all_imgs/*.JPG")
 
     for pic_folder in os.listdir(SD_PATH):
             pictures_dir_path = os.path.join(SD_PATH, pic_folder)
             for pic_name in os.listdir(pictures_dir_path):
                 if ".JPG" in pic_name:
                     pic_path = os.path.join(pictures_dir_path, pic_name)
-                    shutil.copy2(pic_path, 'static/imgs')
+                    shutil.copy2(pic_path, 'static/all_imgs')
