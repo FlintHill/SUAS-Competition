e5366187edfe1d7317dcf31789e0659392a64810
slu11 <47157655+slu11@users.noreply.github.com>
2019-06-09 08:14:19 -0400

    Update gcs.py

--- SUASSystem/SUASSystem/gcs.py
+++ SUASSystem/SUASSystem/gcs.py
@@ -88,6 +88,13 @@ def gcs_process(sda_status, img_proc_status, interop_client_array, targets_to_su
         print("got to if statement before post telem")
         if len(interop_client_array) != 0:
             print("got to post telem statement")
+            
+            
+            if (has_rover_reached_airdrop(rover)):
+                sleep(30)
+                rover.armed = True
+                rover.mode = dronekit.VehicleMode("AUTO")
+             
 
             try:
                 interop_client_array[0].post_telemetry(current_location, vehicle_state_data[0].get_direction())
@@ -196,8 +203,10 @@ def connect_to_vehicle():
     return vehicle
 
 def connect_to_rover():
+    print("Connecting to rover")
     rover = dronekit.connect(GCSSettings.UGV_CONNECTION_STRING, wait_ready=True)
     rover.wait_ready('autopilot_version')
+    print("Connected to rover")
 
     return rover
 
@@ -218,3 +227,19 @@ def has_uav_reached_waypoint(current_location, waypoint_location, threshold_dist
         return True
 
     return False
+
+def has_rover_reached_airdrop(rover):
+    rover_location = converter_functions.get_location(rover)
+    print(rover_location)
+
+    lat_from_drop = abs(GCSSettings.AIRDROP_POINT[0][0] - rover_location.get_lat()) #AIRDOP
+    lon_from_drop = abs(GCSSettings.AIRDROP_POINT[0][1] - rover_location.get_lon())
+    print("Latitude from Drop: " + str(lat_from_drop))
+    print("Longitude from Drop: " + str(lon_from_drop) + "\n")
+
+    if (rover_location.get_alt() < 10 and rover_location.get_alt() > 0
+    and lat_from_drop < 0.00027 and lon_from_drop < 0.00027):
+        return True
+
+    else:
+        return False
