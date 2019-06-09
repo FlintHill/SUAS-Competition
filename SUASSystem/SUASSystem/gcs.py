0c3493d62a33279a39075f7c9bd6b807ab68ab41
slu11 <47157655+slu11@users.noreply.github.com>
2019-06-09 08:21:58 -0400

    Update gcs.py

--- SUASSystem/SUASSystem/gcs.py
+++ SUASSystem/SUASSystem/gcs.py
@@ -34,7 +34,7 @@ def gcs_process(sda_status, img_proc_status, interop_client_array, targets_to_su
     vehicle_state_data = manager.list()
     mission_information_data = manager.list()
     vehicle = connect_to_vehicle()
-    #rover = connect_to_rover()
+    rover = connect_to_rover()
     # SDA
     #waypoints = download_waypoints(vehicle)
     #sda_avoid_coords = manager.list()
